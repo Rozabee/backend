a70e48b42e463d9c8d89ee1eb6656df778c32651
Rozabean <baldonado.beanicole1013@gmail.com>
2025-09-19 00:28:43 +0800

    env

--- server.js
+++ server.js
@@ -18,13 +18,11 @@ app.use(cors());
 
 
 const connection = mysql.createConnection({
-  host: "localhost",
-
-  user: "root",
-
-  password: "",
-
-  database: "brewview_app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 
 });
 
