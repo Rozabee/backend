599c6198aadbf090c7c3a3b2b5cf9ca5a81a3f1d
Rozabean <baldonado.beanicole1013@gmail.com>
2025-09-19 01:25:55 +0800

    muchacho

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use('/images', express.static('public/images'));
 app.use(express.urlencoded({ extended: false }));
+app.use('/images', express.static('public/images'));
 
 const mysql = require("mysql2");
 
